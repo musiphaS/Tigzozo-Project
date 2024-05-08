c361c8a043e078be00200b0fe4ab938a828a7cd2
Kelvin <kelvinmusipha@gmail.com>
2024-05-07 21:17:37 -0700

    images changes

--- tig.js
+++ tig.js
@@ -6,3 +6,20 @@ function toggleDarkMode() {
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+
+    const vector = document.querySelector('.darkmode');
+
+    if (body.classList.contains('dark-mode')) {
+        vector.src = 'darkmode-light.svg.svg'
+    }
+    else {
+        vector.src = 'Vector.svg'
+    }
+
+    // const darkModeimage = document.getElementById('toggle-dark-mode-image');
+    darkModeimage.addEventListener('click', toggleDarkMode);
+}
